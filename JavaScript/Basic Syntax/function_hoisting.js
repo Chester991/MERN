68dc047e17f2188bc08117fb37ff1c157fb23347
Ayush Balwani <ayushbalwani991@gmail.com>
2023-07-19 15:34:19 +0530

    Function Hoisting in JavaScript

---
+++ JavaScript/Basic Syntax/function_hoisting.js
@@ -0,0 +1,12 @@
+function add(){
+   console.log('This Function adds two numbers');
+   return;
+}
+
+var sub = function(){
+   console.log('This function subtracts two numbers');
+   return;
+}
+
+add();
+sub();
