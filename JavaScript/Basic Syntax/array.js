e7085e6f7e74753c1ebb8a8679b1fa8677eed223
Ayush Balwani <ayushbalwani991@gmail.com>
2023-07-19 16:06:29 +0530

    Arrays in JavaScript

---
+++ JavaScript/Basic Syntax/array.js
@@ -0,0 +1,15 @@
+let marks = [12,23,34,55,66];
+console.log(marks);
+
+let output = marks.join(', ');
+console.log(output);
+
+console.log(marks.length);
+
+// iterating through the array
+for(let i=0;i<marks.length;i++){
+   console.log(marks[i]);
+}
+let rem = marks.pop();
+console.log(" ");
+console.log(rem);
